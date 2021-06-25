9f017d7090c7a55ef1fccfa469ad06dc27946c8e
Cqu-yjy <86296730+Cqu-yjy@users.noreply.github.com>
2021-06-25 11:17:51 +0800

    Add files via upload

---
+++ yjy_cot.m
@@ -0,0 +1,25 @@
+function y = yjy_cot(x)
+	if x > 180 
+		quotient = x / 180;  
+		x = x - quotient * 180;
+    end
+    if x < -180   
+		quotient = x / 180;     
+		x = x - quotient * 180;
+    end
+	if x >= 90
+		x = x - 180;
+    end
+    if x <= -90
+		x = x + 180;
+    end
+	newa = x * 3.1415926 / 180.0;
+	y = 1.0 / newa - newa / 3.0 - newa^3 / 45 - 2 * (newa^5) / 945.0;
+	if (-0.007 < y < 0.007)
+		y = 0;
+    end
+end
+
+
+    
+
