5948bf12bc77f63717abab362a0f61415d5095df
Cqu-luj <68043959+Cqu-luj@users.noreply.github.com>
2021-07-04 15:35:18 +0800

    Update lu_cos.m

--- lu_cos.m
+++ lu_cos.m
@@ -1,12 +1,12 @@
 % cos(x) = 1 - x^2 / 2! + x^4 / 4! + ……
 
 function result= lu_cos(input)
-	angle = pi * (input / 180);		    
-	result = 0;  
-	symbol = 1;						
+	angle = pi * (input / 180);	%输入转换为角度	    
+	result = 0;  			
+	symbol = 1;			%符号			
 	sub_result = 1;				
-	denominator = 0;		
-	i=1;
+	denominator = 0;		%展开项中各项的2k
+	i=1;				
 while abs(sub_result) >= 1e-6
     result = result + sub_result;			
     symbol = -symbol;
