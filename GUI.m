e4ca2e5ecdedc4079b192df02fcb246d1bbf6c1a
ZhangYanQi1 <86149561+ZhangYanQi1@users.noreply.github.com>
2021-07-10 22:07:19 +0800

    Update GUI.m

--- GUI.m
+++ GUI.m
@@ -441,4 +441,4 @@ function pushbutton29_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton29 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-    zyq_test();
+    sin_test();
