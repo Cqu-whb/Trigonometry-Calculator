3e0f5e3f94e0a8279526e24db7145b2f66be6fe5
Cqu-luj <68043959+Cqu-luj@users.noreply.github.com>
2021-07-10 19:08:00 +0800

    Update lu_cos.m

--- lu_cos.m
+++ lu_cos.m
@@ -6,11 +6,13 @@ function result= lu_cos(input)
 	symbol = 1;			%符号			
 	sub_result = 1;				
 	denominator = 0;		%展开项中各项的2k
-	i=1;				
+	i=1;	
+	
 while abs(sub_result) >= 1e-6
     result = result + sub_result;			
     symbol = -symbol;
     denominator = denominator + 2	
     sub_result = symbol * angle ^ 2 / factorial(denominator)
 end
+
 result = roundn(result,-2);
